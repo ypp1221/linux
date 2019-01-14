c44437f890a8d5bb730090dca6268f680689e1f9
ypp1221 <xiaoyi1221520@163.com>
2019-01-14 13:23:46 +0800

    append grep

--- 01-linux_find.sh
+++ 01-linux_find.sh
@@ -20,3 +20,12 @@ link source new_link
 链接都不可用
 link -s source new_link
 
+#grep 的使用
+1)查找python版本
+sudo dpkg -l | grep -i python(dpkg -l列出安装的.deb包，-i是忽略大小写)
+2)搜索和过滤文件
+sudo grep -v "#" filepath(-v是输出不匹配项)
+3)查找所有mp3文件,艺术家jayZ的文件
+sudo find . -name ".mp3" |grep -i "jayZ"|grep -vi "remix"
+
+注释：grep -vi "remix"是忽略大小写的查找不包含"remix"的匹配项
