042330282af70b9ecf95d37d618cd4db8c9a95e0
ypp1221 <xiaoyi1221520@163.com>
2019-01-14 13:09:47 +0800

    append 软连接

--- 01-linux_find.sh
+++ 01-linux_find.sh
@@ -11,3 +11,12 @@ sudo find / -iname "*opencv*"
 tar -zxvf xx.tar.gz
 #解压xx.tar.bz2
 tar -jxvf xx.tar.bz2
+
+#硬链接和软连接的区别
+1)硬链接，不能跨分区，不能链接文件夹,如果删除一个链接，不会影响
+该节点的源文件和其他硬链接
+link source new_link 
+2)软连接，可以跨分区，也能链接文件夹，删除源文件，其下所有的软
+链接都不可用
+link -s source new_link
+
